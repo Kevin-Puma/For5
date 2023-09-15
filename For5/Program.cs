042d6eed2b78f7a23fafcfcb6b2b3e99d6aaffeb
Estudiantes <Estudiantes@ULELAB331>
2023-09-15 17:24:54 -0500

    Agregar archivos de proyecto.

---
+++ For5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese cantidad requerida: ");
+int cantidad = int.Parse(Console.ReadLine());  
+
+Random r = new Random();
+
+for (int i = 1; i <= cantidad; i++)
+{
+    int aleatorio = r.Next(1,50+1);
+    Console.WriteLine("Aleatorio: "+i +": "+aleatorio);
+}
